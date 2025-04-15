78c3410be6cdcfa52cfcaa60bca1d6b096592b33
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-15 17:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function trailingZeros(n) {
   //your JS code here. If required.
-	let count =0;
+	 let count = 0;
 
-	fot(let i=5;Math.floor(n/i)>=1;i*=5){
-		count += Math.floor(n/i);
-	}
-	return count;
+  // Count factors of 5, 25, 125, ...
+  for (let i = 5; Math.floor(n / i) >= 1; i *= 5) {
+    count += Math.floor(n / i);
+  }
+
+  return count;
 	
 }
 
