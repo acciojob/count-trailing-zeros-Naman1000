bccaf3206c60ce7a8f5b0211069e8678c5dffc42
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-15 17:35:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let count =0;
+
+	fot(let i=5;Math.floor(n/i)>=1;i*=5){
+		count += Math.floor(n/i);
+	}
+	return count;
+	
 }
 
-const input = prompt("Enter a number");
-alert(trailingZeros(input));
+const input = promp("Enter a number");
+aler(trailingZeros(input));
